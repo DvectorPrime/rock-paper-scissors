2918cdf6428439f878e28808206c4200f244aabf
Dvectorprime <vectorprime2024@gmail.com>
2024-12-05 16:44:38 +0100

    Component Design Complete

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,17 +1,44 @@
 import React from "react";
 import logo from "../images/logo.svg"
+import triangle from "../images/bg-triangle.svg"
+import rock from "../images/icon-rock.svg"
+import paper from "../images/icon-paper.svg"
+import scissors from "../images/icon-scissors.svg"
 
 function Main(){
     return(
-        <header className="header">
-            <div className="logo">
-                <img src={logo} alt="" />
-            </div>
-            <div className="score-container">
-                <p className="score-label">SCORE</p>
-                <p className="score">12</p>
-            </div>
-        </header>
+        <div className="game-container">
+            <header className="header">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="score-container">
+                    <p className="score-label">SCORE</p>
+                    <p className="score">12</p>
+                </div>
+            </header>
+            <main>
+                <img alt="" src={triangle} className="game-triangle" />
+                <button type="button" className="outer-circle paper">
+                    <div className="inner-circle">
+                        <img alt="" src={paper} />
+                    </div>
+                </button>
+                <button type="button" className="outer-circle scissors">
+                    <div className="inner-circle">
+                    <img alt="" src={scissors} />
+                    </div>
+                </button>
+                <button type="button" className="outer-circle rock">
+                    <div className="inner-circle">
+                        <img alt="" src={rock} />
+                    </div>
+                </button>
+            </main>
+            <footer>
+                <button type="button" className="rules">RULES</button>
+            </footer>
+        </div>
     )
 }
 
